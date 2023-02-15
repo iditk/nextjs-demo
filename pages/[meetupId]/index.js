b5c1638bd4846a6b5c17bdefab6eb38c577951a0
Idit <iditked@gmail.com>
2023-02-15 17:36:36 +0200

    fix 6

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -33,7 +33,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
@@ -51,7 +51,7 @@ export async function getStaticProps(context) {
   const meetupsCollection = db.collection("meetups");
 
   const selectedMeetup = await meetupsCollection.findOne({
-    _id: new ObjectId(meetupId),
+    _id: ObjectId(meetupId),
   });
 
   client.close();
